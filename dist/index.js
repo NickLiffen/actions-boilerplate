f1e7b02f5e9f4fc7d60318851ce0171fa7c5453f
Nick Liffen <nickliffen@github.com>
2023-11-05 18:46:37 +0000

    Node20

--- dist/index.js
+++ dist/index.js
@@ -6,12 +6,18 @@
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
 
+Object.defineProperty(exports, "__esModule", ({ value: true }));
 /* Edit Function */
 const run = async () => {
     return;
 };
 run();
+
+})();
 
 module.exports = __webpack_exports__;
 /******/ })()
